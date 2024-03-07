856e86bbba624e68a755f81512f8246b32df1e02
Simon Dahal <simonsd054@gmail.com>
2024-03-07 15:38:35 +1100

    Fix booking table sql and Booking form done

--- booking.php
+++ booking.php
@@ -15,15 +15,93 @@
     <main>
         <div class="card">
             <h1 class="card-header">Booking a cab</h1>
+            <?php
+            // get email from get superglobal from url
+            $email = $_GET["Email"];
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+                // get all fields
+                $name = $_POST["name"];
+                $contact = $_POST["contact"];
+                $unit_number = $_POST["unit_number"];
+                $street_number = $_POST["street_number"];
+                $street_name = $_POST["street_name"];
+                $passenger_suburb = $_POST["passenger_suburb"];
+                $destination_suburb = $_POST["destination_suburb"];
+                $pickup_date = $_POST["pickup_date"];
+                $pickup_time = $_POST["pickup_time"];
+                // check for all the required fields
+                if (
+                    !isset($name) ||
+                    !isset($contact) ||
+                    !isset($street_number) ||
+                    !isset($street_name) ||
+                    !isset($passenger_suburb) ||
+                    !isset($destination_suburb) ||
+                    !isset($pickup_date) ||
+                    !isset($pickup_time) ||
+                    empty($name) ||
+                    empty($contact) ||
+                    empty($street_number) ||
+                    empty($street_name) ||
+                    empty($passenger_suburb) ||
+                    empty($destination_suburb) ||
+                    empty($pickup_date) ||
+                    empty($pickup_time)
+                ) {
+                    $erroMessage = "<span class='error-text'>All fields except unit number are required</span>";
+                } else {
+                    // here if every input field checks out
+
+                    date_default_timezone_set('Australia/Sydney');
+                    $booking_time = date("Y-m-d H:i:s");
+                    $booking_time_str = strtotime($booking_time);
+                    $pickup_full_time = $pickup_date . " " . $pickup_time;
+                    $pickup_time_str = strtotime($pickup_full_time);
+
+
+                    $diff = $pickup_time_str - $booking_time_str;
+
+                    if ($diff >= 2400) {
+
+                        $DBConnect = @mysqli_connect("localhost", "root", "", "taxi_db")
+                            or die("<p>Unable to connect to the database server.</p>" . "<p>Error code " . mysqli_connect_errno() . ": " . mysqli_connect_error()) . "</p>";
+
+                        $SQLstring = "
+                            insert into booking
+                            (email, name, contact, unit_number, street_number, street_name, passenger_suburb, destination_suburb, pickup_time, booking_time) values
+                            ('$email', '$name', '$contact', '$unit_number', '$street_number', '$street_name', '$passenger_suburb', '$destination_suburb', '$pickup_full_time', '$booking_time')";
+
+                        $queryResult = @mysqli_query($DBConnect, $SQLstring)
+                            or die("<p>Unable to query the table.</p>" . "<p>Error code " . mysqli_errno($DBConnect) . ": " . mysqli_error($DBConnect)) . "</p>";
+
+                        $recently_inserted_id = mysqli_insert_id($DBConnect);
+                        $SQLstring = "select * from booking where booking_number='$recently_inserted_id'";
+
+                        $queryResult = @mysqli_query($DBConnect, $SQLstring)
+                            or die("<p>Unable to query the table.</p>" . "<p>Error code " . mysqli_errno($DBConnect) . ": " . mysqli_error($DBConnect)) . "</p>";
+
+                        $queryValue = mysqli_fetch_assoc($queryResult);
+
+                        $booking_number = $queryValue["booking_number"];
+                        echo "<span class='sub-text'>Thank you!
+                            Your booking reference number is $booking_number. We will pick up the
+                            passengers in front of your provided address at $pickup_time on $pickup_date.</span>";
+                    } else {
+                        $erroMessage = "<span class='error-text'>Pickup time must be at least 40 minutes from now.</span>";
+                    }
+                }
+            }
+            ?>
             <p class="sub-text">Please fill the fields below to book a cab</p>
             <form method="post">
                 <div class="form-item">
                     <label class="form-label" for="name">Passenger Name:</label>
-                    <input class="form-input" name="name" placeholder="Name" />
+                    <input class="form-input" id="name" name="name" placeholder="Name" />
                 </div>
                 <div class="form-item">
                     <label class="form-label" for="contact">Passenger Contact:</label>
-                    <input type="number" class="form-input form-input-phone" name="contact" placeholder="Contact (0412345678)" />
+                    <input type="number" class="form-input form-input-phone" id="contact" name="contact" placeholder="Contact (0412345678)" />
                 </div>
 
                 <div class="form-item">
@@ -32,35 +110,39 @@
 
                 <div class="form-item-address">
                     <div class="form-item">
-                        <label class="form-label" for="unit">Unit Number:</label>
-                        <input class="form-input" name="unit" placeholder="Unit Number" />
+                        <label class="form-label" for="unit_number">Unit Number:</label>
+                        <input class="form-input" id="unit_number" name="unit_number" placeholder="Unit Number" />
                     </div>
                     <div class="form-item">
                         <label class="form-label" for="street_number">Street Number:</label>
-                        <input class="form-input" name="street_number" placeholder="Street Number" />
+                        <input class="form-input" id="street_number" name="street_number" placeholder="Street Number" />
                     </div>
                     <div class="form-item">
                         <label class="form-label" for="street_name">Street Name:</label>
-                        <input class="form-input" name="street_name" placeholder="Street Name" />
+                        <input class="form-input" id="street_name" name="street_name" placeholder="Street Name" />
                     </div>
                     <div class="form-item">
-                        <label class="form-label" for="suburb">Suburb:</label>
-                        <input class="form-input" name="suburb" placeholder="Suburb" />
+                        <label class="form-label" for="passenger_suburb">Suburb:</label>
+                        <input class="form-input" id="passenger_suburb" name="passenger_suburb" placeholder="Passenger Suburb" />
                     </div>
                 </div>
 
                 <div class="form-item">
-                    <label class="form-label" for="destination">Destination Suburb:</label>
-                    <input class="form-input" name="destination" placeholder="Destination Suburb" />
+                    <label class="form-label" for="destination_suburb">Destination Suburb:</label>
+                    <input class="form-input" id="destination_suburb" name="destination_suburb" placeholder="Destination Suburb" />
                 </div>
                 <div class="form-item">
-                    <label class="form-label" for="date">Pickup Date:</label>
-                    <input class="form-input" name="date" placeholder="Pickup Date" />
+                    <label class="form-label" for="pickup_date">Pickup Date:</label>
+                    <input type="date" id="pickup_date" class="form-input" name="pickup_date" placeholder="Pickup Date" />
                 </div>
                 <div class="form-item">
-                    <label class="form-label" for="time">Pickup Time:</label>
-                    <input class="form-input" name="time" placeholder="Pickup Time" />
+                    <label class="form-label" for="pickup_time">Pickup Time:</label>
+                    <input type="time" id="pickup_time" class="form-input" name="pickup_time" placeholder="Pickup Time" />
                 </div>
+
+                <?php
+                echo $erroMessage;
+                ?>
 
                 <div class="form-input-buton-div">
                     <button class="form-input-button">Book</button>
--- tables.sql
+++ tables.sql
@@ -2,25 +2,25 @@ CREATE DATABASE tax_db;
 
 CREATE TABLE user (
     email VARCHAR(50) NOT NULL,
-    name VARCHAR(100),
-    password VARCHAR(100),
-    phone VARCHAR(50),
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    phone VARCHAR(50) NOT NULL,
     PRIMARY KEY (email)
 );
 
 CREATE TABLE booking (
     booking_number INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(100) NOT NULL,
-    passenger_name VARCHAR(100),
-    passenger_contact VARCHAR(100),
-    passenger_unit_number VARCHAR(100),
-    passenger_street_number VARCHAR(100),
-    passenger_street_name VARCHAR(100),
-    passenger_suburb VARCHAR(100),
-    destination_suburb VARCHAR(100),
-    pickup_time DATETIME,
-    booking_time DATETIME,
-    status VARCHAR(50) DEFAULT 'unassigned',
+    name VARCHAR(100) NOT NULL,
+    contact VARCHAR(100) NOT NULL,
+    unit_number VARCHAR(10),
+    street_number VARCHAR(10) NOT NULL,
+    street_name VARCHAR(100) NOT NULL,
+    passenger_suburb VARCHAR(50) NOT NULL,
+    destination_suburb VARCHAR(50) NOT NULL,
+    pickup_time DATETIME NOT NULL,
+    booking_time DATETIME NOT NULL,
+    status VARCHAR(50) DEFAULT 'unassigned' NOT NULL,
     PRIMARY KEY (booking_number),
     FOREIGN KEY (email) REFERENCES user(email)
 );
